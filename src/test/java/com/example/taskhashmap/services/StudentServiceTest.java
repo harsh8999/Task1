6cab247df60d3ae24dcf8cd8e01a69dae09a5447
Harsh Nayak <harsh.kumar8999@gmail.com>
2024-01-30 16:01:50 +0530

    test cases added

--- src/test/java/com/example/taskhashmap/services/StudentServiceTest.java
+++ src/test/java/com/example/taskhashmap/services/StudentServiceTest.java
@@ -2,12 +2,14 @@ package com.example.taskhashmap.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.taskhashmap.entity.Student;
+import com.example.taskhashmap.exceptions.ApiException;
 
 @SpringBootTest
 public class StudentServiceTest {
@@ -28,5 +30,35 @@ public class StudentServiceTest {
         assertNotNull(result);
         assertEquals(id, result.getId());
         assertEquals(name, result.getName());
+    }
+
+    @Test
+    public void addStudentWithSameIdThrowException() {
+        Integer id = 1;
+        String name = "Harsh";
+        // add default student
+        service.addStudent(id, name);
+
+        Integer duplicateId = 1;
+        assertThrows(ApiException.class,() -> service.addStudent(duplicateId, name));
+    }
+
+    @Test
+    public void getStudent() {
+        Integer id = 1;
+        String name = "Harsh";
+        Student actual = service.addStudent(id, name);
+
+        Student result = service.getStudent(id);
+        
+        assertNotNull(result);
+        assertEquals(actual.getId(), result.getId());
+        assertEquals(actual.getName(), result.getName());
+    }
+
+    @Test
+    public void studentDontExistThrowException() {
+        
+        assertThrows(ApiException.class,() -> service.getStudent(1));
     }
 }
